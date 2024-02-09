654e645f475cd87fce1f02fd603e4d4a119979c4
Jasper Hale <ljy087621@gmail.com>
2024-02-09 03:11:57 +0000

    FileDescriptor

--- cs144_rust/src/util/file_descriptor.rs
+++ cs144_rust/src/util/file_descriptor.rs
@@ -1,16 +1,21 @@
+use libc::writev;
 use nix::fcntl::{fcntl, FcntlArg, OFlag};
 use nix::libc;
 use nix::unistd::{close, read, write};
-use std::io::{self, ErrorKind};
+use std::cell::RefCell;
+use std::io::{self, ErrorKind, IoSlice};
 use std::os::unix::io::{AsRawFd, RawFd};
+use std::rc::Rc;
 use std::sync::{Arc, Mutex};
+
+use super::buffer::{BufferList, BufferViewList};
 
 // 封装FD的结构体
 #[derive(Debug)]
 struct FDWrapper {
-    fd: RawFd,
-    eof: bool,
-    closed: bool,
+    fd: RawFd,    // create by kernel
+    eof: bool,    // end of file flag
+    closed: bool, // close flag
 }
 
 // 实现FDWrapper
@@ -29,8 +34,9 @@ impl FDWrapper {
 
     fn close(&mut self) {
         if !self.closed {
-            unsafe {
-                close(self.fd).unwrap_or(());
+            let res = close(self.fd);
+            if let Err(e) = res {
+                eprintln!("Failed to close FD: {}", e);
             }
             self.closed = true;
         }
@@ -45,21 +51,24 @@ impl Drop for FDWrapper {
 }
 
 // 共享FDWrapper，提供读写功能
-struct FileDescriptor {
-    internal_fd: Arc<Mutex<FDWrapper>>,
+pub(crate) struct FileDescriptor {
+    internal_fd: Rc<RefCell<FDWrapper>>, // 单线程内部可变
 }
 
 impl FileDescriptor {
-    fn new(fd: RawFd) -> io::Result<Self> {
+    pub(crate) fn new(fd: RawFd) -> io::Result<Self> {
         let fd_wrapper = FDWrapper::new(fd)?;
         Ok(Self {
-            internal_fd: Arc::new(Mutex::new(fd_wrapper)),
+            internal_fd: Rc::new(RefCell::new(fd_wrapper)),
         })
     }
 
-    pub fn read(&self, limit: usize) -> io::Result<Vec<u8>> {
+    pub fn read(&self, mut limit: usize) -> io::Result<Vec<u8>> {
+        if limit > (1024 * 1024) {
+            limit = 1024 * 1024;
+        }
         let mut buffer = vec![0u8; limit];
-        let mut guard = self.internal_fd.lock().unwrap();
+        let mut guard = self.internal_fd.borrow_mut();
 
         if guard.closed {
             Err(io::Error::new(ErrorKind::Other, "FD is closed"))
@@ -74,23 +83,58 @@ impl FileDescriptor {
         }
     }
 
-    pub fn write(&self, buffer: &[u8]) -> io::Result<usize> {
-        let guard = self.internal_fd.lock().unwrap();
+    pub fn write(&self, buffer: &mut BufferViewList, write_all: bool) -> io::Result<usize> {
+        let mut total_bytes_written = 0;
 
-        if guard.closed {
-            Err(io::Error::new(ErrorKind::Other, "FD is closed"))
-        } else {
-            write(guard.fd, buffer).map_err(|e| io::Error::new(ErrorKind::Other, e))
+        while !buffer.is_empty() {
+            let iovecs = buffer.as_io_slices();
+
+            let bytes_written = unsafe {
+                writev(
+                    self.internal_fd.borrow().fd,
+                    iovecs.as_ptr() as *const libc::iovec,
+                    iovecs.len() as libc::c_int,
+                )
+            };
+
+            if bytes_written < 0 {
+                return Err(io::Error::last_os_error());
+            }
+
+            let bytes_written = bytes_written as usize;
+
+            if bytes_written == 0 && !buffer.is_empty() {
+                return Err(io::Error::new(
+                    io::ErrorKind::WriteZero,
+                    "write returned 0 given non-empty input buffer",
+                ));
+            }
+
+            if bytes_written > buffer.len() {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "write wrote more than length of input buffer",
+                ));
+            }
+
+            let _ = buffer.remove_prefix(bytes_written);
+            total_bytes_written += bytes_written;
+
+            if !write_all {
+                break;
+            }
         }
+
+        Ok(total_bytes_written)
     }
 
     pub fn close(&self) {
-        let mut guard = self.internal_fd.lock().unwrap();
+        let mut guard = self.internal_fd.borrow_mut();
         guard.close();
     }
 
     pub fn set_blocking(&self, blocking: bool) -> io::Result<()> {
-        let guard = self.internal_fd.lock().unwrap();
+        let guard = self.internal_fd.borrow();
         let current_flags = fcntl(guard.fd, FcntlArg::F_GETFL)
             .map_err(|e| io::Error::new(ErrorKind::Other, e.to_string()))?;
         let new_flags = if blocking {
@@ -104,6 +148,11 @@ impl FileDescriptor {
         )
         .map_err(|e| io::Error::new(ErrorKind::Other, e.to_string()))
         .map(|_| ())
+    }
+
+    pub fn duplicate(&self) -> io::Result<Self> {
+        let guard = self.internal_fd.clone();
+        Ok(Self { internal_fd: guard })
     }
 }
 
