7c9234399a07f3de3dad2a2cf3f940c0a3734685
Jasper Hale <ljy087621@gmail.com>
2024-02-17 09:31:22 +0000

    Refactor TCPReceiverStateSummary and add TCPState

--- cs144_rust/src/tcp_helpers/tcp_state.rs
+++ cs144_rust/src/tcp_helpers/tcp_state.rs
@@ -1,3 +1,11 @@
+use core::fmt;
+
+use crate::{
+    byte_stream::ByteStreamTrait,
+    tcp_receiver::{TCPReceiver, TCPReceiverTrait},
+};
+
+#[derive(Debug, PartialEq)]
 pub enum TCPReceiverStateSummary {
     Error,
     Listen,
@@ -5,15 +13,35 @@ pub enum TCPReceiverStateSummary {
     FinReceived,
 }
 
-impl ToString for TCPReceiverStateSummary {
-    fn to_string(&self) -> String {
+impl fmt::Display for TCPReceiverStateSummary {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            TCPReceiverStateSummary::Error => "error (connection was reset)".to_string(),
-            TCPReceiverStateSummary::Listen => "waiting for SYN: ackno is empty".to_string(),
+            TCPReceiverStateSummary::Error => write!(f, "error (connection was reset)"),
+            TCPReceiverStateSummary::Listen => write!(f, "waiting for SYN: ackno is empty"),
             TCPReceiverStateSummary::SynReceived => {
-                "SYN received (ackno exists), and input to stream hasn't ended".to_string()
+                write!(
+                    f,
+                    "SYN received (ackno exists), and input to stream hasn't ended"
+                )
             }
-            TCPReceiverStateSummary::FinReceived => "input to stream has ended".to_string(),
+            TCPReceiverStateSummary::FinReceived => write!(f, "input to stream has ended"),
         }
+    }
+}
+
+pub struct TCPState {}
+
+impl TCPState {
+    pub fn state_summary(receiver: &TCPReceiver) -> TCPReceiverStateSummary {
+        if receiver.stream_out().borrow().error() {
+            return TCPReceiverStateSummary::Error;
+        }
+        if receiver.ackno().is_none() {
+            return TCPReceiverStateSummary::Listen;
+        }
+        if receiver.stream_out().borrow().input_ended() {
+            return TCPReceiverStateSummary::FinReceived;
+        }
+        return TCPReceiverStateSummary::SynReceived;
     }
 }
