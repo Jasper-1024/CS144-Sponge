af7e15e9775538f85997a55e6b9e5dbd892bb52f
Jasper Hale <ljy087621@gmail.com>
2024-02-21 13:32:10 +0000

    Refactor TCPReceiverTestHarness in recv_connect.rs

--- cs144_rust/tests/recv_connect.rs
+++ cs144_rust/tests/recv_connect.rs
@@ -83,34 +83,42 @@ fn test_recv_connect() {
         execute_test!(test, ExpectUnassembledBytes, 0);
         execute_test!(test, ExpectTotalAssembledBytes, 0);
     }
-    // {
-    //     let test = TCPReceiverTestHarness::new(5435);
+    {
+        let test = TCPReceiverTestHarness::new(5435);
 
-    //     execute_test!(test, ExpectAckno, None);
-    //     execute_test!(test, ExpectUnassembledBytes, 0);
-    //     execute_test!(test, ExpectTotalAssembledBytes, 0);
+        execute_test!(test, ExpectAckno, None);
+        execute_test!(test, ExpectUnassembledBytes, 0);
+        execute_test!(test, ExpectTotalAssembledBytes, 0);
 
-    //     let seg_arrives = SegmentArrivesBuilder::new()
-    //         .ack(0)
-    //         .fin()
-    //         .seqno(893475)
-    //         .result(Some(Result::NotSyn))
-    //         .build();
-    //     test.execute(&seg_arrives);
+        let mut seg_arrives = SegmentArrivesBuilder::new()
+            .ack(0)
+            .fin()
+            .seqno(893475)
+            .result(Some(Result::NotSyn))
+            .build();
+        let default_segment = seg_arrives.build_segment();
+        test.execute_with_segment(&seg_arrives, &default_segment);
 
-    //     execute_test!(test, ExpectAckno, None);
-    //     execute_test!(test, ExpectUnassembledBytes, 0);
-    //     execute_test!(test, ExpectTotalAssembledBytes, 0);
+        execute_test!(test, ExpectAckno, None);
+        execute_test!(test, ExpectUnassembledBytes, 0);
+        execute_test!(test, ExpectTotalAssembledBytes, 0);
 
-    //     let seg_arrives = SegmentArrivesBuilder::new()
-    //         .syn()
-    //         .seqno(89347598)
-    //         .result(Some(Result::Ok))
-    //         .build();
-    //     test.execute(&seg_arrives);
+        let mut seg_arrives = SegmentArrivesBuilder::new()
+            .syn()
+            .seqno(89347598)
+            .result(Some(Result::Ok))
+            .build();
+        let default_segment = seg_arrives.build_segment();
+        test.execute_with_segment(&seg_arrives, &default_segment);
 
-    //     execute_test!(test, ExpectAckno, Some(89347599));
-    //     execute_test!(test, ExpectUnassembledBytes, 0);
-    //     execute_test!(test, ExpectTotalAssembledBytes, 0);
-    // }
+        execute_test!(test, ExpectAckno, Some(89347599));
+        execute_test!(test, ExpectUnassembledBytes, 0);
+        execute_test!(test, ExpectTotalAssembledBytes, 0);
+    }
+
+    {
+        let cap = u16::MAX as usize + 5;
+        let test = TCPReceiverTestHarness::new(cap);
+        execute_test!(test, ExpectWindow, cap);
+    }
 }
