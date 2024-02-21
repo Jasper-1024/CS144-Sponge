9a5c1c6f0299d4c64f430d9feb02beb1510dca8c
Jasper Hale <ljy087621@gmail.com>
2024-02-21 13:48:03 +0000

    Refactor TCPReceiverTestHarness in recv_close.rs

--- cs144_rust/tests/recv_close.rs
+++ cs144_rust/tests/recv_close.rs
@@ -12,8 +12,7 @@ fn test_recv_close() {
         let isn: u32 = rng.gen_range(0..=std::u32::MAX);
         let test = TCPReceiverTestHarness::new(4000);
 
-        let state = ExpectState::new(TCPReceiverStateSummary::Listen);
-        test.execute(&state);
+        execute_test!(test, ExpectState, TCPReceiverStateSummary::Listen);
 
         let mut seg_arrivers = SegmentArrivesBuilder::new()
             .syn()
@@ -21,11 +20,9 @@ fn test_recv_close() {
             .result(Some(Result::Ok))
             .build();
         let default_segment = seg_arrivers.build_segment();
-
         test.execute_with_segment(&seg_arrivers, &default_segment);
 
-        let state = ExpectState::new(TCPReceiverStateSummary::SynReceived);
-        test.execute(&state);
+        execute_test!(test, ExpectState, TCPReceiverStateSummary::SynReceived);
 
         let mut seg_arrivers = SegmentArrivesBuilder::new()
             .fin()
@@ -33,27 +30,18 @@ fn test_recv_close() {
             .result(Some(Result::Ok))
             .build();
         let default_segment = seg_arrivers.build_segment();
-
         test.execute_with_segment(&seg_arrivers, &default_segment);
 
-        let ackno = ExpectAckno::new((isn + 2).into());
-        test.execute(&ackno);
-
-        let unassembled_bytes = ExpectUnassembledBytes::new(0);
-        test.execute(&unassembled_bytes);
-
-        let bytes = ExpectBytes::new(*b"");
-        test.execute(&bytes);
-
-        let total_assembled_bytes = ExpectTotalAssembledBytes::new(0);
-        test.execute(&total_assembled_bytes);
+        execute_test!(test, ExpectAckno, (isn + 2).into());
+        execute_test!(test, ExpectUnassembledBytes, 0);
+        execute_test!(test, ExpectBytes, *b"");
+        execute_test!(test, ExpectTotalAssembledBytes, 0);
     }
     {
         let isn: u32 = rng.gen_range(0..=std::u32::MAX);
         let test = TCPReceiverTestHarness::new(4000);
 
-        let state = ExpectState::new(TCPReceiverStateSummary::Listen);
-        test.execute(&state);
+        execute_test!(test, ExpectState, TCPReceiverStateSummary::Listen);
 
         let mut seg_arrivers = SegmentArrivesBuilder::new()
             .syn()
@@ -61,11 +49,9 @@ fn test_recv_close() {
             .result(Some(Result::Ok))
             .build();
         let default_segment = seg_arrivers.build_segment();
-
         test.execute_with_segment(&seg_arrivers, &default_segment);
 
-        let state = ExpectState::new(TCPReceiverStateSummary::SynReceived);
-        test.execute(&state);
+        execute_test!(test, ExpectState, TCPReceiverStateSummary::SynReceived);
 
         let mut seg_arrivers = SegmentArrivesBuilder::new()
             .fin()
@@ -74,25 +60,13 @@ fn test_recv_close() {
             .result(Some(Result::Ok))
             .build();
         let default_segment = seg_arrivers.build_segment();
-
         test.execute_with_segment(&seg_arrivers, &default_segment);
 
-        let state = ExpectState::new(TCPReceiverStateSummary::FinReceived);
-        test.execute(&state);
-
-        let ackno = ExpectAckno::new((isn + 3).into());
-        test.execute(&ackno);
-
-        let unsn = ExpectUnassembledBytes::new(0);
-        test.execute(&unsn);
-
-        let bytes = ExpectBytes::new(*b"a");
-        test.execute(&bytes);
-
-        let total_assembled_bytes = ExpectTotalAssembledBytes::new(1);
-        test.execute(&total_assembled_bytes);
-
-        let state = ExpectState::new(TCPReceiverStateSummary::FinReceived);
-        test.execute(&state);
+        execute_test!(test, ExpectState, TCPReceiverStateSummary::FinReceived);
+        execute_test!(test, ExpectAckno, (isn + 3).into());
+        execute_test!(test, ExpectUnassembledBytes, 0);
+        execute_test!(test, ExpectBytes, *b"a");
+        execute_test!(test, ExpectTotalAssembledBytes, 1);
+        execute_test!(test, ExpectState, TCPReceiverStateSummary::FinReceived);
     }
 }
