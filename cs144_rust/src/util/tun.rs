c38de9806b42d9a8fd5c521e2239eced30828b04
Jasper Hale <ljy087621@gmail.com>
2024-04-06 09:04:04 +0000

    Refactor TUN/TAP device creation and configuration

--- cs144_rust/src/util/tun.rs
+++ cs144_rust/src/util/tun.rs
@@ -1,83 +1,87 @@
 use super::file_descriptor::FileDescriptor;
-use nix::fcntl::{self, OFlag};
+use libc::ifreq;
+use nix::fcntl::{open, OFlag};
 use nix::ioctl_write_int;
 use nix::sys::stat;
-use nix::unistd::close;
 use std::ffi::CString;
-use std::os::unix::io::AsRawFd;
+use std::io;
 
+#[warn(dead_code)]
+const CLONEDEV: &str = "/dev/net/tun";
 ioctl_write_int!(ioctl_tun_set_iff, b'T', 202);
-struct TunTapFD {
-    fd: FileDescriptor,
-}
+
+/// A FileDescriptor to a [Linux TUN/TAP](https://www.kernel.org/doc/Documentation/networking/tuntap.txt) device
+struct TunTapFD(FileDescriptor);
 
 impl TunTapFD {
+    /// Open an existing persistent [TUN or TAP device](https://www.kernel.org/doc/Documentation/networking/tuntap.txt).
+    /// \param[in] devname is the name of the TUN or TAP device, specified at its creation.
+    /// \param[in] is_tun is `true` for a TUN device (expects IP datagrams), or `false` for a TAP device (expects Ethernet frames)
+    ///
+    /// To create a TUN device, you should already have run
+    ///
+    ///     ip tuntap add mode tun user `username` name `devname`
+    ///
+    /// as root before calling this function.
     // ref: https://gist.github.com/harrisonturton/f7c8bc2c0396bae08999563840bdf964
-    pub fn new(devname: &str, is_tun: bool) -> Result<Self, String> {
-        fn to_u64_ptr<T>(val: &T) -> u64 {
-            val as *const T as u64
-        }
+    pub fn new(devname: &str, is_tun: bool) -> io::Result<Self> {
         if devname.is_empty() {
-            return Err("Device name must not be empty".to_string());
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Created TunFD with invalid devname",
+            ));
         }
-        // Open the clone device with read/write flags
-        let fd = fcntl::open("/dev/net/tun", OFlag::O_RDWR, stat::Mode::empty())
-            .map_err(|err| format!("Failed to open file with errno {}", err))?;
 
-        // Initialize the ifreq structure for ioctl
-        let mut ifr = libc::ifreq {
-            ifr_ifru: unsafe { std::mem::zeroed() }, // all set to 0
-            ifr_name: [0; libc::IFNAMSIZ],
-        };
+        // open the tun device
+        let fd_raw = open(CLONEDEV, OFlag::O_RDWR, stat::Mode::empty())?;
 
-        // Set the flags for TUN or TAP device
-        ifr.ifr_ifru.ifru_flags = if is_tun {
-            (libc::IFF_TUN | libc::IFF_NO_PI) as i16
+        let mut tun_req: ifreq = unsafe { std::mem::zeroed() };
+
+        tun_req.ifr_ifru.ifru_flags = if is_tun {
+            (libc::IFF_TUN | libc::IFF_NO_PI) as i16 // tun device with no packetinfo
         } else {
             (libc::IFF_TAP | libc::IFF_NO_PI) as i16
         };
 
+        let cstring_devname = CString::new(devname).map_err(|_| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Device name contains a null character",
+            )
+        })?;
+
         // Copy the device name into ifr_name and ensure it's null-terminated
-        let cstring_devname = CString::new(devname).expect("CString::new failed");
-        if cstring_devname.as_bytes().len() >= libc::IFNAMSIZ {
-            panic!("Device name too long");
-        }
-        for (i, &byte) in cstring_devname.as_bytes_with_nul().iter().enumerate() {
-            ifr.ifr_name[i] = byte as i8;
-        }
+        let bytes = cstring_devname.as_bytes_with_nul();
+        let bytes_c_char: &[libc::c_char] =
+            unsafe { &*(bytes as *const [u8] as *const [libc::c_char]) };
+        tun_req.ifr_name[..bytes.len()].copy_from_slice(bytes_c_char);
 
         // Perform the ioctl to create and configure the TUN/TAP device
         unsafe {
-            if let Err(err) = ioctl_tun_set_iff(fd, to_u64_ptr(&ifr)) {
-                let _ = close(fd).map_err(|err| format!("Failed to close file with errno {}", err));
-                return Err(err.to_string());
-            }
+            ioctl_tun_set_iff(fd_raw, &tun_req as *const _ as u64)?;
         }
 
-        Ok(TunTapFD {
-            fd: FileDescriptor::new(fd.as_raw_fd()).unwrap(),
-        })
+        Ok(TunTapFD(FileDescriptor::new_from_rawfd(fd_raw)?))
     }
 }
 
-struct TunFD {
-    fd: TunTapFD,
-}
+/// A FileDescriptor to a [Linux TUN](https://www.kernel.org/doc/Documentation/networking/tuntap.txt) device
+struct TunFD(TunTapFD);
 
 impl TunFD {
-    fn new(devname: &str) -> Result<Self, String> {
+    fn new(devname: &str) -> io::Result<Self> {
         print!("{}", devname);
-        Ok(TunTapFD::new(devname, true).map(|fd| TunFD { fd })?)
+        Ok(TunFD(TunTapFD::new(devname, true)?))
     }
 }
 
-struct TapFD {
-    fd: TunTapFD,
-}
+/// A FileDescriptor to a [Linux TAP](https://www.kernel.org/doc/Documentation/networking/tuntap.txt) device
+struct TapFD(TunTapFD);
 
 impl TapFD {
-    fn new(devname: &str) -> Result<Self, String> {
-        Ok(TunTapFD::new(devname, false).map(|fd| TapFD { fd })?)
+    fn new(devname: &str) -> io::Result<Self> {
+        print!("{}", devname);
+        Ok(TapFD(TunTapFD::new(devname, false)?))
     }
 }
 
