e9e885b4615c91f32cb300bdf4a4ed1f76089aec
Jasper Hale <ljy087621@gmail.com>
2024-04-13 02:45:40 +0000

    Add test cases for SYN sent state

--- cs144_rust/tests/send_connect.rs
+++ cs144_rust/tests/send_connect.rs
@@ -15,14 +15,86 @@ fn test_send_connect() {
         cfg.fixed_isn = Some(WrappingInt32::new(isn));
 
         let mut test = TCPSenderTestHarness::new("SYN sent test", cfg);
-        test.execute(&ExpectState::new(TCPSenderStateSummary::SynSent));
+
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynSent);
         test.execute(
-            &SegmentArrives::new()
+            &ExpectSegment::new()
                 .no_flags()
                 .syn(true)
                 .payload_size(0)
                 .seqno(isn),
         );
-        test.execute(&ExpectBytesInFlight::new(1));
+        execute_test!(test, ExpectBytesInFlight, 1);
+    }
+    {
+        let isn: u32 = rng.gen_range(0..=std::u32::MAX);
+        let mut cfg = TCPConfig::new();
+        cfg.fixed_isn = Some(WrappingInt32::new(isn));
+
+        let mut test = TCPSenderTestHarness::new("SYN sent test", cfg);
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynSent);
+        test.execute(
+            &ExpectSegment::new()
+                .no_flags()
+                .syn(true)
+                .payload_size(0)
+                .seqno(isn),
+        );
+        execute_test!(test, ExpectBytesInFlight, 1);
+        execute_test!(test, AckReceived, WrappingInt32::new(isn + 1));
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynAcked);
+        execute_test!(test, ExpectNoSegment, true);
+        execute_test!(test, ExpectBytesInFlight, 0);
+    }
+    {
+        let isn: u32 = rng.gen_range(0..=std::u32::MAX);
+        let mut cfg = TCPConfig::new();
+        cfg.fixed_isn = Some(WrappingInt32::new(isn));
+
+        let mut test = TCPSenderTestHarness::new("SYN -> wrong ack test", cfg);
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynSent);
+        test.execute(
+            &ExpectSegment::new()
+                .no_flags()
+                .syn(true)
+                .payload_size(0)
+                .seqno(isn),
+        );
+        execute_test!(test, ExpectBytesInFlight, 1);
+        execute_test!(test, AckReceived, WrappingInt32::new(isn));
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynSent);
+        execute_test!(test, ExpectNoSegment, true);
+        execute_test!(test, ExpectBytesInFlight, 1);
+    }
+
+    {
+        let isn: u32 = rng.gen_range(0..=std::u32::MAX);
+        let mut cfg = TCPConfig::new();
+        cfg.fixed_isn = Some(WrappingInt32::new(isn));
+
+        let mut test = TCPSenderTestHarness::new("SYN acked, data", cfg);
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynSent);
+        test.execute(
+            &ExpectSegment::new()
+                .no_flags()
+                .syn(true)
+                .payload_size(0)
+                .seqno(isn),
+        );
+        execute_test!(test, ExpectBytesInFlight, 1);
+        execute_test!(test, AckReceived, WrappingInt32::new(isn + 1));
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynAcked);
+        execute_test!(test, ExpectNoSegment, true);
+        execute_test!(test, ExpectBytesInFlight, 0);
+        test.execute(&WriteBytes::new("abcdefgh".into(), false));
+        test.execute(&Tick::new(1));
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynAcked);
+        test.execute(&ExpectSegment::new().seqno(isn + 1).data("abcdefgh".into()));
+        execute_test!(test, ExpectBytesInFlight, 8);
+        execute_test!(test, AckReceived, WrappingInt32::new(isn + 9));
+        execute_test!(test, ExpectState, TCPSenderStateSummary::SynAcked);
+        execute_test!(test, ExpectNoSegment, true);
+        execute_test!(test, ExpectBytesInFlight, 0);
+        execute_test!(test, ExpectSeqno, WrappingInt32::new(isn + 9));
     }
 }
